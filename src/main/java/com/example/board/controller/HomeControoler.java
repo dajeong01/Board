99e94d02645f7f228a5bb77a548a1d1413b12c61
dajeong01 <wpekwjd@naver.com>
2025-03-24 21:22:48 +0900

    Initial commit

---
+++ src/main/java/com/example/board/controller/HomeControoler.java
@@ -0,0 +1,17 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeControoler {
+	
+	@GetMapping("/")	// 현재경로로 접근
+	public String index() {
+		return "index";
+	}
+	public String a() {
+		return "";
+	}
+}
